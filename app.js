b7ddc490abcee84b2c3dc81274d66189b9960fb2
efosblack <eriamiatoeefe11@gmail.com>
2023-10-30 07:16:11 -0400

    initial commit

---
+++ app.js
@@ -0,0 +1,91 @@
+// Function to fetch and populate research data
+function populateArticleReviews() {
+  const articleReviewList = document.getElementById("article-review-list");
+
+  fetch('articleReviewData.json') 
+    .then((response) => response.json())
+    .then((data) => {
+      data.article_reviews.forEach((item) => {
+        const li = document.createElement("li");
+        li.innerHTML = `
+        <h3 style="color: brown; font-style: italic; font-size: 1.5rem;">${item.title}</h3>
+        <br>
+
+          <p>${item.description}</p>
+          <br>
+          <p style="font-weight: bold;">Publication Date: ${item.publicationDate}</p>
+          <br>
+          <a href="${item.article}" target="_blank">Read Article</a>
+          <a href="${item.critical_appraisal}" target="_blank">Critical Appraisal</a>
+        `;
+        articleReviewList.appendChild(li);
+        
+      });
+    })
+    .catch((error) => console.error('Error:', error));
+    
+}
+function populateResearchWork() {
+  const researchWorkList = document.getElementById("research-work-list");
+
+  fetch('researchWorkData.json') 
+    .then((response) => response.json())
+    .then((data) => {
+      data.research_work.forEach((item) => {
+        const li = document.createElement("li");
+        li.innerHTML = `
+          <h3 style="color: brown; font-style: italic; font-size: 1.5rem;">${item.title}</h3>
+          <br>
+          <p>Authors: ${item.authors}</p>
+          <br>
+          <p style="font-weight: bold;">Publication Date: ${item.publicationDate}</p>
+          <br>
+          <p>${item.abstract}</p>
+          <br>
+          <a href="${item.link}" target="_blank">Read Research Work</a>
+        `;
+        researchWorkList.appendChild(li);
+      });
+    })
+    .catch((error) => console.error('Error:', error));
+};
+
+// Function to handle search
+function handleSearch() {
+  const searchInput = document.getElementById("search").value.toLowerCase();
+  const articleReviewList = document.getElementById("article-review-list");
+  const researchWorkList = document.getElementById("research-work-list");
+
+  // Loop through article reviews and hide/unhide based on search
+  const articleReviewItems = articleReviewList.getElementsByTagName("li");
+  for (const item of articleReviewItems) {
+      const title = item.querySelector("h3").textContent.toLowerCase();
+      if (title.includes(searchInput)) {
+          item.style.display = "block";
+      } else {
+          item.style.display = "none";
+      }
+  }
+
+  // Loop through research work and hide/unhide based on search
+  const researchWorkItems = researchWorkList.getElementsByTagName("li");
+  for (const item of researchWorkItems) {
+      const title = item.querySelector("h3").textContent.toLowerCase();
+      if (title.includes(searchInput)) {
+          item.style.display = "block";
+      } else {
+          item.style.display = "none";
+      }
+  }
+};
+
+
+
+
+
+
+
+
+// Call the function to populate the article review  and research work section
+populateArticleReviews();
+populateResearchWork();
